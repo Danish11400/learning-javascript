cf18a7ff16e3dfcff4b6f924541741b6ba1dd498
Danish nisar <dk41984200@gmail.com>
2025-08-27 15:42:20 +0530

    complete 5 topics of chapter1

---
+++ chapter1/chapter1.js
@@ -0,0 +1,37 @@
+// class 1-4 of chapter1 
+
+let js = 'amazing';
+// if (js === 'amazing') alert('javascript is fun');
+
+console.log('hello world!');
+console.log("Danish Nisar Kumar");
+
+// class5 - of chapter1 - values and variables 
+
+let firstname = "Danish Nisar";
+console.log(firstname);
+let $Hell3o4 = "being human";
+console.log($Hell3o4);
+
+// class7 - of chapter1 - Data Types 
+
+console.log(typeof true);
+console.log(typeof "Danish");
+console.log(typeof 29);
+console.log(typeof false);
+
+let hello = 23;
+console.log(hello);
+
+hello = "Chingam Sir"
+console.log(hello);
+
+hello = true;
+console.log(hello);
+
+hello = "Dr jatka";
+console.log(hello);
+
+
+
+
