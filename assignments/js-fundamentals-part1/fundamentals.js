416c0b3be908c1de9fe7c5618c00b45f503cada3
Danish nisar <dk41984200@gmail.com>
2025-08-29 08:45:35 +0530

    complete 1 more assignment

--- assignments/js-fundamentals-part1/fundamentals.js
+++ assignments/js-fundamentals-part1/fundamentals.js
@@ -1,9 +1,9 @@
-let country = "kashmir";
-let continent = "asia";
-let population = 6900000;
-console.log(country);
-console.log(continent);
-console.log(population);
+// let country = "kashmir";
+// let continent = "asia";
+// let population = 6900000;
+// console.log(country);
+// console.log(continent);
+// console.log(population);
 
 
 let isIsland = false;
@@ -15,3 +15,5 @@ console.log(typeof country);
 console.log(typeof language);
 
 let language = "kashmiri";
+const continent = "asia";
+let country = "kashmir";
