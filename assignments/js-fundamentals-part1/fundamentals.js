304f1062083105d6340c44ff92116f1b2499f13f
Danish nisar <dk41984200@gmail.com>
2025-08-29 16:40:05 +0530

    complete assignment of basic operations

--- assignments/js-fundamentals-part1/fundamentals.js
+++ assignments/js-fundamentals-part1/fundamentals.js
@@ -6,14 +6,42 @@
 // console.log(population);
 
 
-let isIsland = false;
+// let isIsland = false;
 // let language;
 
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
 
 let language = "kashmiri";
 const continent = "asia";
 let country = "kashmir";
+
+
+let country1HalfPopulation = 3450000;
+let country2HalfPopulation = 3450000;
+let population = 6900000;
+population++;
+console.log(population);
+
+
+let finLandpopulation = 6000000;
+console.log(population >= finLandpopulation);
+
+const avergPopulation = 33000000;
+console.log(population <= avergPopulation);
+
+let description =
+    country +
+    "is in " +
+    continent +
+    " and its " +
+    population +
+    " population" +
+    " ,million people speak " +
+    language;
+console.log(description);
+
+
+
