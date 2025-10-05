abc83153207626680390056cb3faa0d431185e70
Danish nisar <dk41984200@gmail.com>
2025-10-05 12:20:44 +0530

    complete project2 of chapter6

--- chapters/chapter6/project2/project2.js
+++ chapters/chapter6/project2/project2.js
@@ -1,1 +1,29 @@
-"use strict";
+"use strict;";
+const openBtn = document.querySelectorAll(".show-model");
+const hiddenDiv = document.querySelector(".hidden");
+const overlay = document.querySelector(".overlay");
+const addinghidden = function () {
+  hiddenDiv.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+const removeHidden = function () {
+  hiddenDiv.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+for (let i = 0; i < openBtn.length; i++) {
+  openBtn[i].addEventListener("click", function () {
+    removeHidden();
+  });
+}
+const showNow = document.querySelector(".cross-btn");
+showNow.addEventListener("click", function () {
+  addinghidden();
+});
+document.addEventListener("keydown", function (hi) {
+  if (hi.key === "Escape") {
+    if (!hiddenDiv.classList.contains("hidden")) {
+      addinghidden();
+    }
+  }
+});
