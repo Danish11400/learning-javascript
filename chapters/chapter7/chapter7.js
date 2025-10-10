3b35ea6545ae1ad95a6a4522ba6ab6676266ba9f
Danish nisar <dk41984200@gmail.com>
2025-10-10 17:33:17 +0530

    complete scope chai practicing

--- chapters/chapter7/chapter7.js
+++ chapters/chapter7/chapter7.js
@@ -1,1 +1,21 @@
 // Class 1-2 - of -chapter7 - How JS Woks Behind
+"use strict";
+
+const hello =21;
+
+if (hello===21){
+    console.log("hello is 21");
+
+
+
+    function add(a,b){
+        return a+b;
+    };
+
+};
+
+
+    console.log(add(2,3));
+
+
+  
