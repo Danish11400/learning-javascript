02b59434a1540fc5f32abaf760a678c416de2474
Danish nisar <dk41984200@gmail.com>
2025-10-12 15:24:49 +0530

    complete 3/4 part of how js works behind

--- chapters/chapter7/chapter7.js
+++ chapters/chapter7/chapter7.js
@@ -1,21 +1,57 @@
 // Class 1-2 - of -chapter7 - How JS Woks Behind
 "use strict";
 
-const hello =21;
+// const hello =21;
 
-if (hello===21){
-    console.log("hello is 21");
+// if (hello===21){
+//     console.log("hello is 21");
+
+// console.log(`hey my age is ${age}`);
+
+// var age =18;
+
+//     function add(a,b){
+
+//         return a+b;
 
 
 
-    function add(a,b){
-        return a+b;
-    };
+//     };
 
-};
+// };
 
 
-    console.log(add(2,3));
+//     console.log(add(2,3));
 
 
   
+const jonas={
+    year:2025,
+    birthyear:2003,
+    calcAge :function(){
+        console.log(this);
+        console.log(2025-this.birthyear);
+    }
+};
+// jonas.calcAge();
+
+  
+const maltida={
+   birthyear:2000,
+
+};
+maltida.calcAge=jonas.calcAge;
+maltida.calcAge();
+jonas.calcAge(); 
+
+
+
+const  addexp= function(a,b){
+    console.log(arguments);
+    return a+b;
+}
+
+console.log(addexp(2,5,4,1));
+
+
+
