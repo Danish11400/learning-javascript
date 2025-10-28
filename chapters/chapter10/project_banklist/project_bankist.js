2bbc387b15fd061ab636de9fdca51a7210721c19
Danish nisar <dk41984200@gmail.com>
2025-10-28 19:20:55 +0530

    complete half challenge2 of chapter10

--- chapters/chapter10/project_banklist/project_bankist.js
+++ chapters/chapter10/project_banklist/project_bankist.js
@@ -6,6 +6,12 @@
 
 /////////////////////////////////////////////////
 // Data
+
+const bankDetails = {
+  name: "pata nahi kuch rakh la gai baad mai",
+  location: "Gulmarg",
+};
+
 const account1 = {
   owner: "Jonas Schmedtmann",
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
@@ -503,7 +509,6 @@ btnClose.addEventListener("click", function (e) {
     );
 
     // Deletes or removes the account from the accounts array
-    accounts.splice(closeAccountIndexNUmber, 1);
 
     // hides the UI after clicking the btnClose
     containerApp.style.opacity = 0;
@@ -534,8 +539,7 @@ btnLoan.addEventListener("click", function (e) {
     updateUI(currentAccount);
   } else if (!approval) {
     // if condition is false
-    alert(`         Not Eligible For That Loan Amount
-           rule: Loan Amount should be equal
+    alert(`Not Eligible For That Loan Amount /n rule: Loan Amount should be equal
            to one deposit Atleast 10% of of 
            Loan amount
       `);
@@ -543,3 +547,81 @@ btnLoan.addEventListener("click", function (e) {
   // clearing input field
   inputLoanAmount.value = "";
 });
+
+// Class24 - Chapter10 - Flat and FlatMap
+
+// Bank personal info
+const overAllBalance = accounts
+  .map((acc) => acc.movements)
+  .flat()
+  .reduce((acmltr, mov) => acmltr + mov, 0);
+console.log(overAllBalance); // result---->17840
+
+bankDetails.bankBalance = overAllBalance;
+
+// Coding Challenge #4
+
+/*
+This time, Julia and Kate are studying the activity levels of different dog breeds.
+
+YOUR TASKS:
+1. Store the the average weight of a "Husky" in a variable "huskyWeight"
+2. Find the name of the only breed that likes both "running" and "fetch" ("dogBothActivities" variable)
+3. Create an array "allActivities" of all the activities of all the dog breeds
+4. Create an array "uniqueActivities" that contains only the unique activities (no activity repetitions). HINT: Use a technique with a special data structure that we studied a few sections ago.
+5. Many dog breeds like to swim. What other activities do these dogs like? Store all the OTHER activities these breeds like to do, in a unique array called "swimmingAdjacent".
+6. Do all the breeds have an average weight of 10kg or more? Log to the console whether "true" or "false".
+7. Are there any breeds that are "active"? "Active" means that the dog has 3 or more activities. Log to the console whether "true" or "false".
+
+BONUS: What's the average weight of the heaviest breed that likes to fetch? HINT: Use the "Math.max" method along with the ... operator.
+
+TEST DATA:
+*/
+
+const breeds = [
+  {
+    breed: "German Shepherd",
+    averageWeight: 32,
+    activities: ["fetch", "swimming"],
+  },
+  {
+    breed: "Dalmatian",
+    averageWeight: 24,
+    activities: ["running", "fetch", "agility"],
+  },
+  {
+    breed: "Labrador",
+    averageWeight: 28,
+    activities: ["swimming", "fetch"],
+  },
+  {
+    breed: "Beagle",
+    averageWeight: 12,
+    activities: ["digging", "fetch"],
+  },
+  {
+    breed: "Husky",
+    averageWeight: 26,
+    activities: ["running", "agility", "swimming"],
+  },
+  {
+    breed: "Bulldog",
+    averageWeight: 36,
+    activities: ["sleeping"],
+  },
+  {
+    breed: "Poodle",
+    averageWeight: 18,
+    activities: ["agility", "fetch"],
+  },
+];
+
+// 1. Store the the average weight of a "Husky" in a variable "huskyWeight"
+
+const huskyIndexNumber = breeds.findIndex((name) => name.breed === "Husky");
+console.log(huskyIndexNumber);
+
+const huskyWeight = breeds.at(huskyIndexNumber).averageWeight;
+console.log(huskyWeight);
+
+// 2. Find the name of the only breed that likes both "running" and "fetch" ("dogBothActivities" variable)
